87e0607474085263d7af729cc652e95a15f711a3
Matteo Muscas <thatman.j@gmail.com>
2019-04-08 09:00:09 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 #Placeholder for a model
 class Recipe < ActiveRecord::Base
 end
+
+<form action="/recipes/<%= @recipe.id %>/delete" method="POST"> 
+<input id="hidden" type="hidden" name="_method" value="delete">
+ <input type="submit" value="delete">
+</form>
