bedc227effa22d5bca2acab1a702e22d4d1f0ab3
Matteo Muscas <thatman.j@gmail.com>
2019-04-08 09:00:28 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,4 @@
 class Recipe < ActiveRecord::Base
 end
 
-<form action="/recipes/<%= @recipe.id %>/delete" method="POST">
-<input id="hidden" type="hidden" name="_method" value="delete">
- <input type="submit" value="delete">
-</form>
+
